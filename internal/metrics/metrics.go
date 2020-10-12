1ead5ef88ea20e2e0302404961ffca9c4eda5fdf
Levi Harrison <levisamuelharrison@gmail.com>
2020-10-12 09:34:23 -0400

    Fixed some words

--- internal/metrics/metrics.go
+++ internal/metrics/metrics.go
@@ -7,13 +7,13 @@ import (
 var (
 	// TXMinute1 is the average kilobits of traffic per second transmitted in the last minute
 	TXMinute1 = prometheus.NewGauge(prometheus.GaugeOpts{
-		Name: "fios_tx_kb/s_minute_1",
+		Name: "fios_tx_kbs_minute_1",
 		Help: "Average kilobits of traffic per second transmitted in the last minute.",
 	})
 
 	// RXMinute1 is the average kilobits of traffic per second received in the last minute
 	RXMinute1 = prometheus.NewGauge(prometheus.GaugeOpts{
-		Name: "fios_rx_kb/s_minute_1",
+		Name: "fios_rx_kbs_minute_1",
 		Help: "Average kilobits of traffic per second received in the last minute.",
 	})
 )
